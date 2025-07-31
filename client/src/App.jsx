fc1194ab0e5eff4c768f5319de36629e8caf27fc
Srikanth771 <srikanthofficial1910@gmail.com>
2025-07-31 18:00:04 +0530

    menu-management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Home } from "./pages/user/home/Home";
 import Cart from "./pages/user/cart/Cart";
+import MenuManagement from "./components/admin/menuManagement/MenuManagement";
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<  Cart/>}/>
+         <Route path="/menu" element={<MenuManagement/>}/>
+        
         
       </Routes>
     </BrowserRouter>
