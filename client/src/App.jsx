521468c23f05fd544df9183518772475856b6592
VeereshaS431 <tridizi.dev1@gmail.com>
2025-08-01 13:53:17 +0530

    new changes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/user/home/Home";
+import  Home from "./pages/user/home/Home";
 import Dashboard from "./components/admin/menuManagement/dashboard/Dashboard";
 import OrderHistory from "./components/admin/menuManagement/orderhistory/OrderHistory";
 // import { useState } from "react";
